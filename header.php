f27d9ecf81c98c00fa9536c1597af74baed9d737
high5 <high525jp@gmail.com>
2015-02-08 01:30:11 +0900

    update

--- header.php
+++ header.php
@@ -35,18 +35,22 @@
     <div class="blog-masthead">
       <div class="container">
         <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
+          <a class="blog-nav-item active" href="#">Menlo Park</a>
+          <!--
           <a class="blog-nav-item" href="#">New features</a>
           <a class="blog-nav-item" href="#">Press</a>
           <a class="blog-nav-item" href="#">New hires</a>
           <a class="blog-nav-item" href="#">About</a>
+          -->
         </nav>
       </div>
     </div>
 
-    <div class="container">
+    <div class="container" style="padding-top: 30px;">
 
+      <!--
       <div class="blog-header">
         <h1 class="blog-title"><?php bloginfo('name'); ?></h1>
         <p class="lead blog-description"><?php bloginfo('description'); ?></p>
       </div>
+      -->
--- sidebar.php
+++ sidebar.php
@@ -1,30 +1,45 @@
         <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
           <div class="sidebar-module sidebar-module-inset">
             <h4>About</h4>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <p>I'm japanese. Live in Tokyo.</p>
           </div>
-          <div class="sidebar-module">
-            <h4>Archives</h4>
-            <ol class="list-unstyled">
-              <li><a href="#">March 2014</a></li>
-              <li><a href="#">February 2014</a></li>
-              <li><a href="#">January 2014</a></li>
-              <li><a href="#">December 2013</a></li>
-              <li><a href="#">November 2013</a></li>
-              <li><a href="#">October 2013</a></li>
-              <li><a href="#">September 2013</a></li>
-              <li><a href="#">August 2013</a></li>
-              <li><a href="#">July 2013</a></li>
-              <li><a href="#">June 2013</a></li>
-              <li><a href="#">May 2013</a></li>
-              <li><a href="#">April 2013</a></li>
-            </ol>
-          </div>
-          <div class="sidebar-module">
-            <h4>Elsewhere</h4>
-            <ol class="list-unstyled">
+
+
+        <?php if ( ! dynamic_sidebar( 'primary' ) ) : ?>
+
+            <div id="search" class="sidebar-module">
+                <?php get_search_form(); ?>
+            </div>
+
+            <div id="categories" class="sidebar-module"><h4>Categories</h4>
+                <ol class="list-unstyled">
+
+                    <?php wp_list_categories( 'title_li=' ); ?>
+                </ol>
+            </div>
+
+            <div id="archives" class="sidebar-modulet"><h4>Archives</h4>
+                <ol class="list-unstyled">
+                    <?php wp_get_archives( 'type=monthly' ); ?>
+                </ol>
+            </div>
+
+            <div id="subscribe" class="sidebar-module"><h4>Subscribe</h4>
+                <ol class="list-unstyled">
+                    <li><a href="<?php bloginfo( 'rss2_url' ); ?>">Entries (RSS)</a></li>
+                    <li><a href="<?php bloginfo( 'comments_rss2_url' ); ?>">Comments (RSS)</a></li>
+                </ol>
+            </div>
+
+           <div class="sidebar-module">
+              <h4>Elsewhere</h4>
+              <ol class="list-unstyled">
               <li><a href="http://github.com/high5">GitHub</a></li>
               <li><a href="http://twitter.com/high5">Twitter</a></li>
-            </ol>
-          </div>
+              </ol>
+            </div>
+
+
+        <?php endif; ?>
+
         </div><!-- /.blog-sidebar -->
